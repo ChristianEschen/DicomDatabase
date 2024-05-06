9e5b5b0224fc8f13beafbad68ea7f3737f0ce02b
Christian Eschen <christian_eschen@hotmail.com>
2024-05-06 21:51:08 +0200

    update to windows deploy

--- deploy/simulate_deployment.sh
+++ deploy/simulate_deployment.sh
@@ -3,10 +3,10 @@
 
 database="mydb"
 num_workers=2
-input_folder="/home/alatar/miacag/data/angio/sample_data2"
-dataset_folder="/home/alatar/miacag/data/angio"
+input_folder="/deploy/deploy_ext/docker_files/data/sample_data"
+dataset_folder="/deploy/deploy_ext/docker_files/data"
 
-temp_dir="/home/alatar/DicomDatabase/temp_dir"
+temp_dir="/deploy/deploy_ext/docker_files/temp_dir"
 para_method="concurrent_futures"
 dicom_reader_backend="pydicom"
 username="alatar"
@@ -15,9 +15,9 @@ host="localhost"
 port=5432
 table_name="test_deploy"
 master_port=1237
-config_path_lca_rca="/home/alatar/DicomDatabase/lca_rca.yaml"
-config_path_rca_sten="/home/alatar/DicomDatabase/classification_config_angio_SEP_Apr26_12-52-33/classification_config_angio.yaml"
-new_output_path=/home/alatar/DicomDatabase/output_data
+config_path_lca_rca="/deploy/deploy_ext/docker_files/lca_rca.yaml"
+config_path_rca_sten="/deploy/deploy_ext/docker_files/classification_config_angio_SEP_Apr26_12-52-33/classification_config_angio.yaml"
+new_output_path="/deploy/deploy_ext/docker_files/output_folder"
 master_addr="127.0.1.1"
 schema_name="cag"
 export DB_HOST=my-custom-postgres-instance
@@ -38,7 +38,7 @@ python BuildDatabase.py --database $database \
 sed -i "s|^[[:space:]]*output:.*|output: \"$new_output_path\"|" "$config_path_lca_rca"
 sed -i "s|^[[:space:]]*DataSetPath:.*|DataSetPath: \"$dataset_folder\"|" "$config_path_lca_rca"
 
-export PYTHONPATH="${PYTHONPATH}:/home/alatar/miacag"
+export PYTHONPATH="${PYTHONPATH}:/deploy/miacag"
 
 # TODO maybe normalize data before GridPatchD 
 torchrun --nproc_per_node=1 \
@@ -46,7 +46,7 @@ torchrun --nproc_per_node=1 \
 --node_rank=0 \
 --master_addr=$master_addr \
 --master_port=$master_port \
-/home/alatar/miacag/miacag/scripts/stenosis_regression/submit_angio.py \
+/deploy/miacag/miacag/scripts/stenosis_regression/submit_angio.py \
 --cpu "False" \
 --num_workers $num_workers \
 --config_path $config_path_lca_rca \
@@ -75,9 +75,22 @@ torchrun --nproc_per_node=1 \
 --node_rank=0 \
 --master_addr=$master_addr \
 --master_port=$master_port \
-/home/alatar/miacag/miacag/scripts/pretraining_downstreams/submit_angio.py \
+/deploy/miacag/miacag/scripts/pretraining_downstreams/submit_angio.py \
 --cpu "False" \
 --num_workers $num_workers \
 --config_path $config_path_rca_sten \
 --table_name_input $table_name
+
+
+
+# torchrun --nproc_per_node=1 \
+# --nnodes=1 \
+# --node_rank=0 \
+# --master_addr=$master_addr \
+# --master_port=$master_port \
+# /deploy/miacag/miacag/scripts/stenosis_regression/submit_angio.py \
+# --cpu "False" \
+# --num_workers $num_workers \
+# --config_path $config_path_rca_sten \
+# --table_name_input $table_name
 
